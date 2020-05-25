7cfad8e3fd3392846dd938234e9265c9b1683729
bilal khan <billalkhn98@gmail.com>
2020-05-25 18:45:10 +0500

    Repository pattern work

---
+++ BulkyBook.DataAccess/Repository/IRepository/IUnitOfWork.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace BulkyBook.DataAccess.Repository.IRepository
+{
+   public  interface IUnitOfWork : IDisposable
+    {
+        ICategoryRepository Category { get; }
+        SP__Call SP__Call { get; }
+
+
+
+    }
+}
